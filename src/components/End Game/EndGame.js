f7ac838ee42b035f973a80b1aec3046e49365c8e
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-04-22 12:01:06 -0600

    Add functionality to determine amount correct

--- src/components/End Game/EndGame.js
+++ src/components/End Game/EndGame.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const EndGame = ({ clearInterval, myInterval}) => { 
-  
+const EndGame = ({ userGuess, isItSwiftOrSpeare, displayedQuotes, setIsGameStarted }) => {
+  const [numberCorrect, setNumberCorrect] = useState(0)
+
+  useEffect(() => {
+    for (var i = 0; i < userGuess.length; i++) {
+      if (userGuess[i] === isItSwiftOrSpeare[i]) {
+        setNumberCorrect(previous => previous + 1)
+      }
+    }
+  }, [])
+
   return (
     <div>
       <h1> Game over!</h1>
-      <button onCLick={() => clearInterval(myInterval) }>view</button>
+      <h1>HERES MORE THINGS</h1>
+      <h3>
+        you got {numberCorrect}/{userGuess.length} correct!
+      </h3>
+      <Link to="/">
+        <button onClick={() => setIsGameStarted(false)}>Play Again!</button>
+      </Link>
     </div>
   )
 }
