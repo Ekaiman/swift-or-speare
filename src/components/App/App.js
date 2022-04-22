6e2e6f691638cd25ae4d44ca9c90ba86796c3fc5
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-04-22 12:00:37 -0600

    Reset state after game ends

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,84 +2,79 @@
 import './App.css'
 import React, { useEffect, useState } from 'react'
 import apiCalls from '../../apiCalls'
-import { Route, Switch, Link } from 'react-router-dom'
+import { Route, Routes, Switch, Link, useNavigate } from 'react-router-dom'
 import GameScreen from '../GameScreen/GameScreen'
 import EndGame from '../End Game/EndGame'
+import shakeSpeareQuotes from '../../shakeSpearData'
 
 function App() {
-  var myInterval
-
   //>>>>>>>>>>>>STATE<<<<<<<<<<<<<<
   const [timer, setTimer] = useState(30)
-  const [endGame, setEndGame] = useState(false)
+  const [isEndGame, setIsEndGame] = useState(false)
   const [isGameStarted, setIsGameStarted] = useState(false)
   const [randomQuote, setRandomQuote] = useState('')
-  const [isItSwift, setIsitSwift] = useState()
+  const [isItSwiftOrSpeare, setIsitSwiftOrSpeare] = useState([])
   const [displayedQuotes, setDisplayedQuotes] = useState([])
-  const [guess, setGuess] = useState([])
+  const [userGuess, setUserGuess] = useState([])
   const [taylorSwift, setTaylorSwift] = useState([])
-  const [shakeSpeare, setShakeSpeare] = useState([
-    'Love all, trust a few, do wrong to none.',
-    'Love looks not with the eyes, but with the mind.',
-    'Be not afraid of greatness. Some are born great, some achieve greatness, and others have greatness thrust upon them.',
-    'There is nothing either good or bad, but thinking makes it so.',
-    'Hell is empty and all the devils are here.',
-    'We know what we are, but not what we may be.',
-    'You speak an infinite deal of nothing.',
-    'The course of true love never did run smooth.',
-    "Don't waste your love on somebody, who doesn't value it.",
-    'Expectation is the root of all heartache.',
-    'Listen to many, speak to a few.',
-    'One may smile, and smile, and be a villain.',
-    "What's done cannot be undone.",
-    'All the worlds a stage, and all the men and women merely players. They have their exits and their entrances; and one man in his time plays many parts.',
-    'All that glisters is not gold.',
-    'I like this place and could willingly waste my time in it.'
-  ])
+  const [shakeSpeare, setShakeSpeare] = useState(shakeSpeareQuotes)
 
   //>>>>>>>USEEFECT>><<<<<<<<<<<<
   useEffect(() => {
     apiCalls
       .fetchData('https://taylorswiftapi.herokuapp.com/get-all?album=fearless')
       .then(data => setTaylorSwift(data))
-  }, [])
+    }, [])
+    
+    useEffect(() => {
+      if (!isGameStarted) {
+      setTimer(30)
+      setIsEndGame(false)
+      setIsGameStarted(false)
+      setUserGuess([])
+      setDisplayedQuotes([])
+      setIsitSwiftOrSpeare([])
+      
+    }
+   }, [isGameStarted])
 
   useEffect(() => {
     if (timer <= 0) {
-      setEndGame(true)
-  
+      setIsEndGame(true)
     }
   }, [timer])
 
   useEffect(() => {
-    if (!endGame && isGameStarted) {
+    if (!isEndGame && isGameStarted) {
       const interval = setInterval(() => {
         let newTime = timer - +1
         setTimer(previousTime => previousTime - 1)
       }, 1000)
       return () => clearInterval(interval)
     }
-  }, [endGame, isGameStarted])
+  }, [isEndGame, isGameStarted])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const endGamePage = () => navigate('/game-over')
+    if (isEndGame) {
+      endGamePage()
+    }
+  }, [isEndGame])
 
   const swiftOrSpeare = () => {
-    let swift = 0
-    let speare = 0
-    swift = Math.floor(Math.random() * 10)
-    speare = Math.floor(Math.random() * 10)
+    let swift = Math.floor(Math.random() * 10)
+    let speare = Math.floor(Math.random() * 10)
     if (swift > speare) {
       let index = Math.floor(Math.random() * 20)
-      console.log(index)
-      console.log(taylorSwift)
       setRandomQuote(taylorSwift[index].quote)
-      setIsitSwift(true)
-      console.log(taylorSwift[index].quote)
+      setIsitSwiftOrSpeare(past => [...past, 'Swift'])
       cleanQuote(index)
     } else {
       let index = Math.floor(Math.random() * shakeSpeare.length)
       setRandomQuote(shakeSpeare[index])
-      setIsitSwift(false)
-      console.log(shakeSpeare[index])
-      console.log(swift, speare)
+      setIsitSwiftOrSpeare(past => [...past, 'Speare'])
     }
   }
 
@@ -93,42 +88,61 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>swift or speare</header>
-      <Switch>
-        <Route exact path='/game-begin'>
-          <GameScreen
-            randomQuote={randomQuote}
-            isItSwift={isItSwift}
-            setDisplayedQuotes={setDisplayedQuotes}
-            swiftOrSpeare={swiftOrSpeare}
-            setGuess={setGuess}
-          />
-        </Route>
-        <Route exact path='/'>
-          <button
-            onClick={() => {
-              console.log('hi')
-              setTimer(10)
-            }}
-          >
-            10 seconds
-          </button>
-          <button onClick={() => setTimer(20)}>20 seconds</button>
-          <button onClick={() => setTimer(30)}>30 seconds</button>
-          <Link to='/game-begin'>
-            <button
-              onClick={() => {
-                swiftOrSpeare()
-                setIsGameStarted(true)
-              }}
-            >
-              Start Game
-            </button>
-          </Link>
-        </Route>
-        <Route to='game-over'>
-          <EndGame myInterval={myInterval} clearInterval={clearInterval} />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route
+          exact
+          path='/game-begin'
+          element={
+            <GameScreen
+              randomQuote={randomQuote}
+              isItSwiftOrSpeare={isItSwiftOrSpeare}
+              setDisplayedQuotes={setDisplayedQuotes}
+              swiftOrSpeare={swiftOrSpeare}
+              setUserGuess={setUserGuess}
+            />
+          }
+        />
+        <Route
+          exact
+          path='/'
+          element={
+            <div>
+              <button
+                onClick={() => {
+                  console.log('hi')
+                  setTimer(10)
+                }}
+              >
+                10 seconds
+              </button>
+              <button onClick={() => setTimer(20)}>20 seconds</button>
+              <button onClick={() => setTimer(30)}>30 seconds</button>
+              <Link to='/game-begin'>
+                <button
+                  onClick={() => {
+                    swiftOrSpeare()
+                    setIsGameStarted(true)
+                  }}
+                >
+                  Start Game
+                </button>
+              </Link>
+            </div>
+          }
+        />
+        <Route
+          exact
+          path='/game-over'
+          element={
+            <EndGame
+              displayedQuotes={displayedQuotes}
+              isItSwiftOrSpeare={isItSwiftOrSpeare}
+              userGuess={userGuess}
+              setIsGameStarted={setIsGameStarted}
+            />
+          }
+        />
+      </Routes>
     </div>
   )
 }
