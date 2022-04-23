42f61db9df76190337a6960dd6419408e34d1c9c
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-04-23 17:37:32 -0600

    Fix doubling number bug

--- src/components/End Game/EndGame.js
+++ src/components/End Game/EndGame.js
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const EndGame = ({ userGuess, isItSwiftOrSpeare, displayedQuotes, setIsGameStarted }) => {
+const EndGame = ({
+  userGuess,
+  isItSwiftOrSpeare,
+  displayedQuotes,
+  setIsGameStarted,
+  ranOutOfQuotes,
+  isEndGame
+}) => {
   const [numberCorrect, setNumberCorrect] = useState(0)
 
   useEffect(() => {
-    for (var i = 0; i < userGuess.length; i++) {
-      if (userGuess[i] === isItSwiftOrSpeare[i]) {
-        setNumberCorrect(previous => previous + 1)
+    if (isEndGame) {
+      for (var i = 0; i < userGuess.length; i++) {
+        if (userGuess[i] === isItSwiftOrSpeare[i]) {
+          setNumberCorrect(previous => previous + 1)
+        }
       }
     }
-  }, [])
+  }, [isEndGame])
 
   return (
     <div>
       <h1> Game over!</h1>
-      <h1>HERES MORE THINGS</h1>
+      {ranOutOfQuotes && <p>You're too good! We ran out of quotes.</p>}
       <h3>
         you got {numberCorrect}/{userGuess.length} correct!
       </h3>
-      <Link to="/">
+      <Link to='/'>
         <button onClick={() => setIsGameStarted(false)}>Play Again!</button>
       </Link>
     </div>
