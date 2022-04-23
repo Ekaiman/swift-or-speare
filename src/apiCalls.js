b5eaeb32e6e6b9c1b07a3c66acfe203c9beee4d1
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-04-23 17:36:14 -0600

    Remove comments

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,6 +7,3 @@ const apiCalls = {
 }
 
 export default apiCalls
-
-// 'https://taylorswiftapi.herokuapp.com/get'
-// curl --location --request GET 'https://shakespeare-quotes-generator.herokuapp.com/api/v1/quotes/single'
